1b64654d965975b4c43f990b6ecb02a1e9ec9709
danieleangeletti <angelettidaniele@gmail.com>
2023-12-13 16:17:45 +0100

    versione provvisoria completata

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -5,11 +5,23 @@ my_button.addEventListener('click', function() {
     for (i = 1; i < 101; i++) {
 
         const main = document.getElementById('grid');
+        main.classList.add("bg-success");
+        main.classList.add("bg-opacity-50");
         const cell = document.createElement("div");
         cell.classList.add("square");
+        cell.classList.add("border");
+        cell.classList.add("border-black");
+        cell.classList.add("text-center");
         cell.innerHTML = i;
         main.append(cell);
-        
+
+        cell.addEventListener('click', function(){
+            this.classList.toggle("bg-primary");
+            console.log(cell.innerHTML);
+        })
+
     }
 
 })
+
+
