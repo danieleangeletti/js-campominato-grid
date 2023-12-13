8fc6053b0b469cef36f754fc1aca2e8452e341cf
danieleangeletti <angelettidaniele@gmail.com>
2023-12-13 16:00:02 +0100

    griglia creata

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,10 +1,15 @@
 const my_button = document.getElementById("play-button");
 
 my_button.addEventListener('click', function() {
+
     for (i = 1; i < 101; i++) {
+
+        const main = document.getElementById('grid');
         const cell = document.createElement("div");
-        const main = document.querySelector("main div");
-        main.append(cell);
+        cell.classList.add("square");
         cell.innerHTML = i;
+        main.append(cell);
+        
     }
+
 })
