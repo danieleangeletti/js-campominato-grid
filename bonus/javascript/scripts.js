58d046dfa69511f2470446ac63095588dc3880e7
danieleangeletti <angelettidaniele@gmail.com>
2023-12-13 17:25:41 +0100

    bonus completato

--- bonus/javascript/scripts.js
+++ bonus/javascript/scripts.js
@@ -1,46 +1,76 @@
-// const my_button = document.getElementById("play-button");
-
-// my_button.addEventListener('click', function() {
-
-//     for (i = 1; i < 101; i++) {
-
-//         const main = document.getElementById('grid');
-//         const cell = document.createElement("div");
-//         cell.classList.add("square");
-//         cell.classList.add("border");
-//         cell.classList.add("border-black");
-//         cell.classList.add("text-center");
-//         cell.innerHTML = i;
-//         main.append(cell);
-
-//         cell.addEventListener('click', function(){
-//             this.classList.toggle("bg-primary");
-//             console.log(cell.innerHTML);
-//         })
-
-//     }
-
-// })
-
-// my_button.addEventListener("click", function() {
-//     const main = document.getElementById("grid");
-//     user_choice = document.createElement("select");
-//     user_choice.classList.add("form-select");
-//     user_choice.innerHTML = `
-//         <option selected>Select the difficulty level</option>
-//         <option value="1">Easy</option>
-//         <option value="2">Medium</option>
-//         <option value="3">Hard</option>
-//     `
-//     main.append(user_choice);
-// })
-
 const difficulty = document.getElementById("choice-of-difficulty");
 
 const my_button = document.getElementById("play-button");
 
 my_button.addEventListener("click", function(){
-    console.log(difficulty.value);
+
+    easy_medium_hard = parseInt(difficulty.value);
+
+    if (easy_medium_hard == 1) {
+
+        for (i = 1; i < 101; i++) {
+
+            const main = document.getElementById('grid');
+            main.classList.add("pt-4");
+            main.classList.add("pb-4");
+            const cell = document.createElement("div");
+            cell.classList.add("square");
+            cell.classList.add("border");
+            cell.classList.add("border-black");
+            cell.classList.add("text-center");
+            cell.innerHTML = i;
+            main.append(cell);
+
+            cell.addEventListener("click", function(){
+                this.classList.toggle("bg-primary");
+                console.log(cell.innerHTML);
+            })
+        }
+    }
+
+    else if (easy_medium_hard == 2) {
+        
+        for (i = 1; i < 82; i++) {
+
+            const main = document.getElementById('grid');
+            main.classList.add("pt-4");
+            main.classList.add("pb-4");
+            const cell = document.createElement("div");
+            cell.classList.add("square-2");
+            cell.classList.add("border");
+            cell.classList.add("border-black");
+            cell.classList.add("text-center");
+            cell.innerHTML = i;
+            main.append(cell);
+
+            cell.addEventListener("click", function(){
+                this.classList.toggle("bg-primary");
+                console.log(cell.innerHTML);
+            })
+        }
+    }
+
+    else if (easy_medium_hard == 3) {
+
+        for (i = 1; i < 50; i++) {
+
+            const main = document.getElementById('grid');
+            main.classList.add("pt-4");
+            main.classList.add("pb-4");
+            const cell = document.createElement("div");
+            cell.classList.add("square-3");
+            cell.classList.add("border");
+            cell.classList.add("border-black");
+            cell.classList.add("text-center");
+            cell.innerHTML = i;
+            main.append(cell);
+
+            cell.addEventListener("click", function(){
+                this.classList.toggle("bg-primary");
+                console.log(cell.innerHTML);
+            })
+        }
+    }
 })
 
 
